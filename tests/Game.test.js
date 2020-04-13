d5d790fe26f105f13222d0895b956a3c0db2d0b1
Rex <rexpeters3n@gmail.com>
2020-04-12 19:58:27 -0500

    update header test

--- tests/Game.test.js
+++ tests/Game.test.js
@@ -7,13 +7,8 @@ import RejectionButton from '../components/RejectionButton';
 describe('<Game />', () => {
   it('has 1 child', () => {
     const tree = renderer.create(<RejectionButton />).toJSON();
-    console.log(tree);
     expect(tree).toMatchSnapshot();
     expect(tree.children.length).toBe(1);
-  });
-
-  it('can take the value X', () => {
-    const testInstance = renderer.create(<RejectionButton />).toJSON();
   });
 
 });
--- tests/Header.Test.js
+++ tests/Header.Test.js
@@ -7,9 +7,13 @@ import { Header } from 'react-native/Libraries/NewAppScreen';
 describe('<Header />', () => {
   it('has 1 child', () => {
     const tree = renderer.create(<Header/>).toJSON();
-    console.log(tree);
     expect(tree).toMatchSnapshot();
     expect(tree.children.length).toBe(1);
+  });
+
+  it('can take the status ', () => {
+    const testInstance = renderer.create(<Header status={'foo'}/>).toJSON();
+    expect(testInstance.children[0].children[0]).toEqual('foo');
   });
 
 });
